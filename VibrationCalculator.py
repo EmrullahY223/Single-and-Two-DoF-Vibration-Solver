81e0bb230a059996ed0ff907979af8c8b836561b
Emrullah_Y <emrullah2234@outlook.com>
2023-06-30 11:58:39 +0300

    Update VibrationCalculator.py
    example Problems are added

--- VibrationCalculator.py
+++ VibrationCalculator.py
@@ -4,46 +4,40 @@ numerik yöntemler ile titreşim analizi için yazılmıştır.
 """
 import math
 def step_size(k1,m1,k2,m2,tfin):
-    k = 0
-    m = 0
 
-    if k2 > k1:
-        k = k2
-    else:
-        k = k1
-    if m2 > m1:
-        m = m2
-    else:
-        m = m1
+    num_of_division = 40
+    natural_frequancy1 = 0
+    natural_frequancy2 = 0
+    nat_freq = 0
 
-    dt = float(1/(((k/m)**0.5)/(2*math.pi))/50)
-    print(f"dt : {dt}")
+    if m1 > 0:
+        natural_frequancy1 = (k1/m1)**0.5
+    if m2 > 0:
+        natural_frequancy2 = (k2/m2)**0.5
+
+    if natural_frequancy1 > natural_frequancy2:
+        nat_freq = natural_frequancy1
+    else:
+        nat_freq = natural_frequancy2
+
+    dt = float((1/(nat_freq/(2*math.pi)))/num_of_division)
     num_of_steps = round((tfin/dt)+1)
+    print(f"dt:{dt}")
     return num_of_steps, dt
 
 def MovingBase(baseMag,baseFreq,dt,num_of_steps,m2,movetype,stepSlope,stepEnd,times):
     Base = [0]*(num_of_steps+1)
     BaseSpeed = [0]*(num_of_steps+1)
     t  = [0]
-    num_of_stepsBetweenSeconds = int(1/dt)
+    
     if m2 <= 0:
         if movetype == 1:
             for i in range(0,num_of_steps):
                 Base[i] = baseMag*math.sin(baseFreq*t[i])
                 BaseSpeed[i] = baseMag*baseFreq*math.cos(baseFreq*t[i])
                 t.append(t[i]+dt)
-        elif movetype == 2:
-            
-            y = 0 * num_of_stepsBetweenSeconds
-            for i in range(y,num_of_steps+1):
-                Base[i] = stepSlope * t[i]
-                print(i)
-                BaseSpeed[i-1] = (Base[i]-Base[i-1])/dt
-                t.append(t[i]+dt)
-                if Base[i] > stepEnd:
-                    break
         else:
-            print(f"dafuk mate")
+            print(f"move type worng for base")
     else:
         print(f"can't have base mass with moving base!!")
 
@@ -52,35 +46,38 @@ def MovingBase(baseMag,baseFreq,dt,num_of_steps,m2,movetype,stepSlope,stepEnd,ti
 def forceCalc(type1,type2,mag1,mag2,freq1,freq2,times1,times2,num_of_steps,dt):
     F1 = [0] * (num_of_steps+1)
     F2 = [0] * (num_of_steps+1)
-    t  = [0] 
+    
     num_of_stepsBetweenSeconds = int(1/dt)
-    for i in range(0,num_of_steps):
-        t.append(t[i]+dt)
-    if type1 == 1:
+        
+    if type1 == 1: # harmonic excitation for m1
+        t  = [0] 
         for i in range(0,len(F1)):
             F1[i] = mag1*math.sin(freq1*t[i])
-            
-    elif type1 ==2:
+            t.append(t[i]+dt)
+
+    elif type1 ==2: # impulse excitation for m1
         for i in times1:
             print(i)
             F1[round(i*num_of_stepsBetweenSeconds)] = mag1/dt
     else:
-        print("dafuk")
-    if type2 == 1:
+        print("error in excitation type1")
+
+    if type2 == 1:  # harmonic excitation for m2
+        t  = [0] 
         for i in range(0,len(F1)):
             F2[i] = mag2*math.sin(freq2*t[i])
-            
-    elif type2 ==2:
+            t.append(t[i]+dt)
+
+    elif type2 ==2: # impulse excitation for m2
         for i in times2:
             F2[i*num_of_stepsBetweenSeconds] = mag2/dt
     else:
-        print("dafuk2")
+        print("error in excitation type2")
     return F1,F2
     
 def Solver(x1_0,v1_0,a1_0,x2_0,v2_0,a2_0,F1,F2,m1,m2,c1,c2,k1,k2,num_of_steps,Base,BaseSpeed):
-    
+    # defining the variables
     t  = [0]
-    print(f"number of steps: {num_of_steps}")
     X1 = [0] *(num_of_steps+1)
     V1 = [0] *(num_of_steps+1)
     A1 = [0] *(num_of_steps+1)
@@ -91,9 +88,11 @@ def Solver(x1_0,v1_0,a1_0,x2_0,v2_0,a2_0,F1,F2,m1,m2,c1,c2,k1,k2,num_of_steps,Ba
     A2 = [0] *(num_of_steps+1)
     G2 = [0] *(num_of_steps+1)
     
+    # defining the Boundary Conditions
     X1[0] = x1_0
     V1[0] = v1_0
     A1[0] = a1_0
+
     if m2 <= 0:
         X2 = Base
         V2 = BaseSpeed
@@ -103,7 +102,7 @@ def Solver(x1_0,v1_0,a1_0,x2_0,v2_0,a2_0,F1,F2,m1,m2,c1,c2,k1,k2,num_of_steps,Ba
         V2[0] = v2_0
         A2[0] = a2_0
     
-    # loop for numerical analysis
+    # loop for numerical solution
     for i in range(0,num_of_steps):
         #print (f"i={i}")
         t.append(t[i]+dt)
@@ -118,7 +117,6 @@ def Solver(x1_0,v1_0,a1_0,x2_0,v2_0,a2_0,F1,F2,m1,m2,c1,c2,k1,k2,num_of_steps,Ba
             V2[i+1] = (-2*m2*X2[i] + 2*m2*X2[i+1] + G2[i]*dt**2 - k2*dt**2*X2[i+1])/(2*m2*dt + c2*dt**2)
             A2[i+1] = (G2[i] - c2*V2[i+1] - k2*X2[i+1])/m2
         
-    
     return X1,X2,V1,V2,A1,A2,G1,G2,t
 
 if __name__ == "__main__":
@@ -138,7 +136,7 @@ if __name__ == "__main__":
     times = [0]
     v2_0 = 0.0
     a2_0 = 0.0
-    type1 = 1
+    type1 = 2
     type2 = 2
     times1 = [0]
     times2 = [0]
@@ -146,22 +144,30 @@ if __name__ == "__main__":
     mag2 = 0.0 #float(input("Enter force magnitude: "))
     freq1 = 0.0 #float(input("Enter force frequency: "))
     freq2 = 0.0
-    m1 = 0.5 #float(input("Enter mass: "))
+    m1 = 1 #float(input("Enter mass: "))
     m2 = 0.0
-    c1 = 0.1 #float(input("Enter damper: "))
+    c1 = -4 #float(input("Enter damper: "))
     c2 = 0.0
-    k1 = 3.0 #float(input("Enter spring: "))
+    k1 = 3 #float(input("Enter spring: "))
     k2 = 0.0
-    tfin = 10.0 #float(input("Enter final time: "))
+    tfin = 1.0 #float(input("Enter final time: "))
 
     num_of_steps,dt = step_size(k1,m1,k2,m2,tfin)
     F1,F2 = forceCalc(type1,type2,mag1,mag2,freq1,freq2,times1,times2,num_of_steps,dt)
     Base,BaseSpeed = MovingBase(baseMag,baseFreq,dt,num_of_steps,m2,movetype,stepSlope,stepEnd,times)
     X1,X2,V1,V2,A1,A2,G1,G2,t = Solver(x1_0,v1_0,a1_0,x2_0,v2_0,a2_0,F1,F2,m1,m2,c1,c2,k1,k2,num_of_steps,Base,BaseSpeed)
-    #print(F1)
-
-    
+    #print(V1)
+    exactSolution = list()
+    # example Problem 1
+    if m1 == 570.69 and c1 == 1819.245 and k1 == 1.2e7 and mag1 == 220.0 and type1 == 2:
+        for i in t:
+            solution = 2.69e-3*(math.e**(-1.59*i))*math.sin(144.9*i)
+            exactSolution.append(solution)
+    # example Problem 2
+    else:
+        print("dafuk")
     plt.plot(t,X1)
+    #plt.plot(t,exactSolution)
     #plt.plot(t,Base)
     if m2 <=0 :
         print("no second Dof/moving Base")

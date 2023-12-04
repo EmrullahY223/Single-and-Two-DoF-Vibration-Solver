886a268781f7644c6f81e90f807231ccdc58b176
Emrullah_Y <emrullah2234@outlook.com>
2023-12-04 21:43:27 +0300

    RK4
    Integrator turned into Runge - Kutta 4 Method

--- SDOF_Integrator.py
+++ SDOF_Integrator.py
@@ -1,27 +1,53 @@
 """
 This file is created as a newer version of the older Numerical Integration Algorithm, for a clean start
+
+Using State-Space Form
+
 """
 
 import numpy as np
 from numpy.linalg import inv
 from matplotlib import pyplot as plt
+
+def F(t):
+    F = np.array([0.0,0.0])
+
+    if t <= 15:
+        F[0] = F0 * np.cos(omega*t)
+    else:
+        F[0] = 0.0
+    
+    return F
+
+def G(y,t):
+    return A_inv.dot(F(t) - B.dot(y))
+
+def RK4_step(y,t,delta_t):
+
+    K1 = G(y,t)
+    K2 = G(y+(K1*delta_t/2), t+delta_t/2)
+    K3 = G(y+(K2*delta_t/2), t+delta_t/2)
+    K4 = G(y+(K3*delta_t), t+delta_t)
+
+    return delta_t/6*(K1 + 2*K2 + 2*K3 + K4)
+
 #Variables
 m = 2.0
 k = 2.0
-c = 1.0     # critical damping = 2 * SQRT(m*k)
+c = 0.0     # critical damping = 2 * SQRT(m*k)
 
-F0 = 1.0
-delta_t = 0.001
+F0 = 0.0
+delta_t = .1
 omega = 1.0
 time = np.arange(0.0,60.0,delta_t)
 
 #Initial Conditions
 
-y = np.array([0,0])     #[velocity, displacement]
+y = np.array([0,1])     #[velocity, displacement]
 
 A = np.array([[m,0],[0,1]])
 B = np.array([[c,k],[-1,0]])
-F = np.array([0.0,0.0])
+A_inv = inv(A)
 
 Y = []
 force = []
@@ -29,14 +55,9 @@ force = []
 #Time-Stepping Solution
 for t in time:
     
-    if t <= 15:
-        F[0] = F0 * np.cos(omega*t)
-    else:
-        F[0] = 0.0
-    
-    y = y + delta_t*inv(A).dot(F - B.dot(y))
+    y = y + RK4_step(y,t,delta_t)
     Y.append(y[1])
-    force.append(F[0])
+    force.append(F(t)[0])
 
     KE = 0.5 * m * y[0]**2
     PE = 0.5 * k * y[1]**2
@@ -47,10 +68,8 @@ for t in time:
 
 #Plot the Results
 
-t = [i for i in time]
-
-plt.plot(t,Y)
-plt.plot(t,force)
+plt.plot(time,Y)
+plt.plot(time,force)
 plt.grid(True)
 plt.legend(['Displacement','Force'], loc='lower right')
 
